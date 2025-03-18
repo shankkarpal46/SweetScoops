e6362e90ebae33d1dca7128073cba5cae05a5b9b
shankkarpal46 <shankkarpal46@gmail.com>
2025-03-18 10:34:34 +0530

    minor changes

--- sweetscoops/sweetscoops/views.py
+++ sweetscoops/sweetscoops/views.py
@@ -8,7 +8,7 @@ from .forms import CustomUserCreationForm,CustomUserChangeForm
 
 def home(request):
     top_icecreams=Tag.objects.get(id=1).icecreams.all()
-    # top_icecreams=Tag.objects.get(id=7).icecreams.all()
+    #top_icecreams=Tag.objects.get(id=7).icecreams.all()
     return render(request,"index.html",{"categories":Category.objects.all(),"top_icecreams":top_icecreams})
 
 def dishes(request):
